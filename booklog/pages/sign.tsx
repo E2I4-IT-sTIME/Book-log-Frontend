1087fa427ebadf9e8e5bae80536950bbb81816b3
EUNA <beni1026@naver.com>
2022-04-10 03:01:09 +0900

    fix : login css, footer

--- booklog/pages/sign.tsx
+++ booklog/pages/sign.tsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import Login from "../components/Login";
+import Signup from "../components/Signup";
 
 export default function sign() {
+  const [isSign, setIsSign] = useState(false);
+  const changeSignPage = () => {
+    setIsSign(true);
+  }
   return (
     //로그인, 회원가입
     <>
-      <Login />
+      {!isSign ? <Login onChange={changeSignPage} /> : <Signup /> }
     </>
   );
 }
