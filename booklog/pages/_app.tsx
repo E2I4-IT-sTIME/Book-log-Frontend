9e675a4f41a9a436d97dc831b6db08cfd520c44f
EUNA <beni1026@naver.com>
2022-05-17 16:28:53 +0900

    fix : 푸터 위치, html height

--- booklog/pages/_app.tsx
+++ booklog/pages/_app.tsx
@@ -3,9 +3,20 @@ import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <style global jsx>{`
+        html,
+        body,
+        div#__next {
+          height: 100%;
+          width: 100%;
+          margin: 0px;
+        }
+      `}</style>
+    </>
   );
 }
 
