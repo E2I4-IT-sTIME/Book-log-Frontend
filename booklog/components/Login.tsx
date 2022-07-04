c368adedc707de8556aeb34ef08b50eb7bb53cb1
Jun99uu <igun0423@naver.com>
2022-07-05 01:33:08 +0900

    독서모임 생성 : 상태관리

--- booklog/components/Login.tsx
+++ booklog/components/Login.tsx
@@ -43,8 +43,8 @@ const Login: NextPage<{ onChange: () => void }> = (props) => {
       .post(
         "http://3.39.152.5:8080/login",
         {
-          username: "userEmail",
-          password: "userPassword",
+          username: loginData.Email,
+          password: loginData.password,
         },
         {
           headers: {
@@ -64,68 +64,93 @@ const Login: NextPage<{ onChange: () => void }> = (props) => {
   return (
     <>
       <div className="login_background">
-          <div className="form_div">
-            <div>
-              <div className="logodiv"><Image src={logo} alt="Booklog logo" objectFit="contain" width="300px" height="58px" /></div>
-              <form onSubmit={onSubmitHandler}>
-                <input
-                  className="text_box"
-                  type="userEmail"
-                  placeholder="이메일 주소"
-                  onChange={userEmailChangeHandler}
-                ></input>
-                <br />
-                <input
-                  className="text_box"
-                  type="password"
-                  placeholder="비밀번호"
-                  onChange={passwordChangeHandler}
-                ></input>
-                <br />
-                <input id="userEmail-save" type="checkbox"></input>
-                <label className="emailsave" htmlFor="userEmail-save">이메일 저장</label>
-                <br />
-                <button className="loginBtn" onClick={() => loginHandler}>로그인</button>
-              </form>
-              <div className="login">
-                <p className="other_login">다른 서비스 계정으로 로그인</p>
-                <div className="googleBtn">
-                  <Image className="img" src={google} objectFit="contain" width="25px"></Image>
-                  <div className="btntext">구글 계정으로 로그인</div>
-                </div>
-                <div className="kakaoBtn">
-                  <Image className="img" src={kakao} objectFit="contain" width="25px"></Image>
-                  <div className="btntext">카카오 계정으로 로그인</div>                 
-                </div>
-                <div className="naverBtn">
-                  <Image  className="img" src={naver} objectFit="contain" width="25px"></Image>
-                  <div className="btntext">네이버 계정으로 로그인</div>
-                </div>
-              </div>
-
-              <hr />
+        <div className="form_div">
+          <div>
+            <div className="logodiv">
+              <Image
+                src={logo}
+                alt="Booklog logo"
+                objectFit="contain"
+                width="300px"
+                height="58px"
+              />
             </div>
-            <div className="others">
-              <div className="otherBtn">
-                  비밀번호 재설정
+            <form onSubmit={onSubmitHandler}>
+              <input
+                className="text_box"
+                type="userEmail"
+                placeholder="이메일 주소"
+                onChange={userEmailChangeHandler}
+              ></input>
+              <br />
+              <input
+                className="text_box"
+                type="password"
+                placeholder="비밀번호"
+                onChange={passwordChangeHandler}
+              ></input>
+              <br />
+              <input id="userEmail-save" type="checkbox"></input>
+              <label className="emailsave" htmlFor="userEmail-save">
+                이메일 저장
+              </label>
+              <br />
+              <button className="loginBtn" onClick={() => onSubmitHandler}>
+                로그인
+              </button>
+            </form>
+            <div className="login">
+              <p className="other_login">다른 서비스 계정으로 로그인</p>
+              <div className="googleBtn">
+                <Image
+                  className="img"
+                  src={google}
+                  objectFit="contain"
+                  width="25px"
+                ></Image>
+                <div className="btntext">구글 계정으로 로그인</div>
               </div>
-              <div className="otherBtn">
-                <a onClick={props.onChange}>회원 가입</a>
+              <div className="kakaoBtn">
+                <Image
+                  className="img"
+                  src={kakao}
+                  objectFit="contain"
+                  width="25px"
+                ></Image>
+                <div className="btntext">카카오 계정으로 로그인</div>
               </div>
+              <div className="naverBtn">
+                <Image
+                  className="img"
+                  src={naver}
+                  objectFit="contain"
+                  width="25px"
+                ></Image>
+                <div className="btntext">네이버 계정으로 로그인</div>
+              </div>
+            </div>
+
+            <hr />
+          </div>
+          <div className="others">
+            <div className="otherBtn">비밀번호 재설정</div>
+            <div className="otherBtn">
+              <a onClick={props.onChange}>회원 가입</a>
             </div>
           </div>
+        </div>
       </div>
       <style jsx>{`
-        .login_background{
+        .login_background {
           width: 500px;
           margin: 0px auto;
           display: flex;
           align-items: center;
           justify-content: center;
         }
-        .logodiv{
-          width:100%;
-          text-align:center;
+        .logodiv {
+          width: 100%;
+          text-align: center;
           margin-bottom: 40px;
         }
 
@@ -134,16 +159,15 @@ const Login: NextPage<{ onChange: () => void }> = (props) => {
           box-sizing: border-box;
           width: 80%;
           height: 100%;
-
         }
         .text_box {
           width: 97%;
           height: 25px;
           margin-bottom: 10px;
-          padding-left:5px;
+          padding-left: 5px;
         }
         .emailsave {
-          font-size:13px;
+          font-size: 13px;
         }
 
         .login {
@@ -157,7 +181,7 @@ const Login: NextPage<{ onChange: () => void }> = (props) => {
           display: flex;
           justify-content: center;
           align-items: center;
-          background-color: #324A86;
+          background-color: #324a86;
           color: white;
           border: 0px;
           border-radius: 5px;
@@ -171,93 +195,87 @@ const Login: NextPage<{ onChange: () => void }> = (props) => {
 
         .googleBtn {
           width: 70%;
-          background-color: #ECECEC;
-          height:40px;
-          display:flex;
-          align-items: center;
-          justify-content: center;
-          flex-direction: row;
-          margin-bottom: 10px;
-          font-size:13px;
-          cursor:pointer;
-        }
-        .kakaoBtn{
-          width: 70%;
-          background-color: #F4DF4B;
-          height:40px;
-          
-          display:flex;
-          align-items: center;
-          justify-content: center;
-          flex-direction: row;
-          margin-bottom: 10px;
-          font-size:13px;
-          cursor:pointer;
-        }
-        .naverBtn{
-          width: 70%;
-          background-color: #30C71F;
-          height:40px;
-          
-          display:flex;
-          align-items: center;
-          justify-content: center;
-          flex-direction: row;
-          font-size:13px;
-          cursor:pointer;
-        }
-        .btntext{
-          width:80%;
-          height:100%;
-          text-align:center;
+          background-color: #ececec;
+          height: 40px;
           display: flex;
           align-items: center;
           justify-content: center;
-          margin-left:8px;
-          border-left : 1px solid white;
+          flex-direction: row;
+          margin-bottom: 10px;
+          font-size: 13px;
+          cursor: pointer;
         }
-       
-        .others{
+        .kakaoBtn {
+          width: 70%;
+          background-color: #f4df4b;
+          height: 40px;
+
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-direction: row;
+          margin-bottom: 10px;
+          font-size: 13px;
+          cursor: pointer;
+        }
+        .naverBtn {
+          width: 70%;
+          background-color: #30c71f;
+          height: 40px;
+
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-direction: row;
+          font-size: 13px;
+          cursor: pointer;
+        }
+        .btntext {
+          width: 80%;
+          height: 100%;
+          text-align: center;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          margin-left: 8px;
+          border-left: 1px solid white;
+        }
+
+        .others {
           width: 100%;
-          display:flex;
+          display: flex;
           align-items: center;
           justify-content: space-between;
           flex-direction: row;
         }
 
-        hr{
+        hr {
           margin: 20px 0;
         }
-        .other_login{
-          font-size:20px;
-          font-weight:600;
+        .other_login {
+          font-size: 20px;
+          font-weight: 600;
         }
-        
-        .otherBtn{
+
+        .otherBtn {
           width: 48%;
           height: 45px;
-          
 
           display: flex;
           align-items: center;
           justify-content: center;
 
-          background-color: #B9C4E0;
-          color:white;
+          background-color: #b9c4e0;
+          color: white;
           font-size: 18px;
           font-weight: 600;
-          cursor:pointer;
-          border-radius: 5px;          
+          cursor: pointer;
+          border-radius: 5px;
         }
-        Link{
-          text-decoration: none; 
-          color: inherit; 
+        Link {
+          text-decoration: none;
+          color: inherit;
         }
-        
-        
-
-
-
       `}</style>
     </>
   );
---
+++ booklog/components/MakeClubQuestion.tsx
@@ -0,0 +1,301 @@
+import { useRecoilState } from "recoil";
+import { recoilCreateBookClubState } from "../states/recoilCreateBookClub";
+import { useState, useEffect } from "react";
+import { ComponentProps, DOMAttributes } from "react";
+import Router from "next/router";
+
+type EventHandlers<T> = Omit<
+  DOMAttributes<T>,
+  "children" | "dangerouslySetInnerHTML"
+>;
+
+export type Event<
+  TElement extends keyof JSX.IntrinsicElements,
+  TEventHandler extends keyof EventHandlers<TElement>
+> = ComponentProps<TElement>[TEventHandler];
+
+interface CreateBookClubState {
+  name: string;
+  img: string;
+  onoff: boolean;
+  max_num: number;
+  tag: Array<string>;
+  content: string;
+  welcome: string;
+  question: Array<string>;
+}
+
+interface stepProps {
+  nextSteps: () => void;
+}
+
+export default function MakeClubQuestion(props: stepProps) {
+  const { nextSteps } = props;
+  const [recoilInfo, setRecoilInfo] = useRecoilState(recoilCreateBookClubState);
+  const defaultState: CreateBookClubState = { ...recoilInfo };
+  const [welcome, setWelcome] = useState("");
+  const [question, setQuestion] = useState("");
+  const [questions, setQuestions] = useState<string[]>([]);
+  const router = Router;
+
+  const inputHandler: Event<"input", "onChange"> = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+    switch (name) {
+      case "welcome":
+        if (welcome.length < 100) setWelcome(value);
+        break;
+      case "question":
+        if (question.length < 100) setQuestion(value);
+        break;
+      default:
+        alert("잘못된 접근입니다.");
+        break;
+    }
+  };
+
+  const addQuestionBtn = () => {
+    if (question !== "") {
+      setQuestions((prev) => [...prev, question]);
+      setQuestion("");
+    }
+  };
+
+  const deleteQuestion = (ind: number) => {
+    let filtered = questions.filter((question, index) => index !== ind);
+    setQuestions(filtered);
+  };
+
+  const cancel = () => {
+    if (
+      confirm(
+        "모임 생성 작업을 정말 취소하시겠습니까?\n작성하던 내용은 저장되지 않습니다."
+      )
+    )
+      router.back();
+  };
+
+  const nextBtn = () => {
+    if (welcome !== "" && questions.length !== 0) {
+      if (
+        confirm(
+          "다음단계로 이동하시겠습니까?\n작성하던 내용은 수정할 수 없습니다."
+        )
+      )
+        saveState();
+    } else {
+      alert("모든 정보를 입력해주세요.");
+    }
+  };
+
+  const saveState = () => {
+    defaultState.welcome = welcome;
+    defaultState.question = questions;
+    setRecoilInfo(defaultState);
+    nextSteps();
+  };
+
+  return (
+    <div className="container">
+      <span className="title">
+        모임 신청자에게 궁금한 것이 있다면, 물어보세요!
+      </span>
+      <div className="box">
+        <div className="content">
+          <div className="welcome-box">
+            <span className="subtitle">가입환영 멘트</span>
+            <form onSubmit={(e) => e.preventDefault()}>
+              <input
+                type="text"
+                placeholder="모임 신청자들에게 간단한 가입 환영 멘트를 작성해주세요!"
+                value={welcome}
+                name="welcome"
+                onChange={inputHandler}
+              />
+            </form>
+          </div>
+          <div className="question-box">
+            <div>
+              <span className="subtitle">가입질문</span>
+              <span className="additional">
+                모임 신청하는 분에게 물어보고 싶은 질문을 작성해보세요! ( 최소
+                1개부터 최대 3개까지 작성 가능해요.)
+              </span>
+            </div>
+            <div className="question-list">
+              {questions.map((q, index) => (
+                <form onSubmit={(e) => e.preventDefault()} key={index}>
+                  <span className="q-title">질문{index + 1}.</span>
+                  <input type="text" value={q} disabled />
+                  <button onClick={() => deleteQuestion(index)}>
+                    - 질문 삭제하기
+                  </button>
+                </form>
+              ))}
+              {questions.length < 5 ? (
+                <form onSubmit={(e) => e.preventDefault()}>
+                  <span className="q-title">질문{questions.length + 1}.</span>
+                  <input
+                    type="text"
+                    placeholder="ex. 가장 좋아하는 책의 제목은 무엇인가요?"
+                    name="question"
+                    value={question}
+                    onChange={inputHandler}
+                  />
+                  <button onClick={() => addQuestionBtn()}>
+                    + 질문 추가하기
+                  </button>
+                </form>
+              ) : (
+                <></>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="next-btns">
+        <button onClick={() => cancel()}>취소</button>
+        <button onClick={() => nextBtn()}>다음</button>
+      </div>
+      <style jsx>{`
+        .container {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          gap: 20px;
+          padding-top: 50px;
+        }
+
+        .title {
+          font-size: 32px;
+          color: #324a86;
+          font-weight: bold;
+        }
+
+        .box {
+          width: 85%;
+          background-color: #e3ebff;
+          border-radius: 100px;
+          padding: 100px 0px 100px 0px;
+          display: flex;
+          justify-content: center;
+        }
+
+        .content {
+          width: 80%;
+          display: flex;
+          flex-direction: column;
+          gap: 20px;
+        }
+
+        .subtitle {
+          font-size: 1.5rem;
+        }
+
+        .welcome-box,
+        .question-box {
+          display: flex;
+          flex-direction: column;
+          gap: 8px;
+        }
+
+        .additional {
+          padding-left: 10px;
+          color: #324a86;
+        }
+
+        .welcome-box input {
+          width: 98%;
+          height: 25px;
+          padding: 5px 10px 5px 10px;
+          background-color: white;
+          border-radius: 0.5em;
+          border: 1px solid #000;
+          box-shadow: 0 5px 18px 0px rgba(50, 50, 93, 0.111),
+            0 3px 10px -3px rgba(0, 0, 0, 0.137),
+            0 -1px 8px -1px rgba(0, 0, 0, 0.025);
+        }
+
+        .question-box {
+          width: 100%;
+          display: flex;
+          flex-direction: column;
+          gap: 15px;
+        }
+
+        .question-list {
+          display: flex;
+          flex-direction: column;
+          gap: 15px;
+          justify-content: center;
+        }
+
+        .question-list form {
+          display: flex;
+          flex-direction: row;
+          gap: 8px;
+          align-items: center;
+          justify-content: space-between;
+        }
+
+        .question-list input {
+          width: 80%;
+          height: 25px;
+          padding: 5px 10px 5px 10px;
+          background-color: white;
+          border-radius: 0.5em;
+          border: 1px solid #000;
+          box-shadow: 0 5px 18px 0px rgba(50, 50, 93, 0.111),
+            0 3px 10px -3px rgba(0, 0, 0, 0.137),
+            0 -1px 8px -1px rgba(0, 0, 0, 0.025);
+        }
+        .q-title {
+          font-size: 1.2rem;
+        }
+
+        .question-list button {
+          border: none;
+          background-color: #cdd6f0;
+          height: 40px;
+          padding: 0px 16px 0px 15px;
+          border-radius: 10px;
+          color: #324a86;
+          font-weight: bold;
+          box-shadow: 0 5px 18px 0px rgba(50, 50, 93, 0.111),
+            0 3px 10px -3px rgba(0, 0, 0, 0.137),
+            0 -1px 8px -1px rgba(0, 0, 0, 0.025);
+          cursor: pointer;
+        }
+
+        .next-btns {
+          display: flex;
+          flex-direction: row;
+          gap: 10px;
+          margin: auto;
+          padding-bottom: 100px;
+        }
+        .next-btns button {
+          padding: 10px 25px 10px 25px;
+          border-radius: 0.5rem;
+          border: 2px solid #324a86;
+          background-color: white;
+          color: #324a86;
+          font-weight: bold;
+          font-size: 1rem;
+          box-shadow: 0 5px 18px 0px rgba(50, 50, 93, 0.111),
+            0 3px 10px -3px rgba(0, 0, 0, 0.137),
+            0 -1px 8px -1px rgba(0, 0, 0, 0.025);
+          transition: 0.25s;
+          cursor: pointer;
+        }
+
+        .next-btns button:hover {
+          border: 2px solid white;
+          background-color: #324a86;
+          color: white;
+        }
+      `}</style>
+    </div>
+  );
+}
