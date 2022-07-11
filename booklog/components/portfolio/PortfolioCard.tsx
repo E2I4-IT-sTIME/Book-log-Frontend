b592a51b98f12f16748ba99652228f0623952d74
EUNA <beni1026@naver.com>
2022-07-11 13:45:55 +0900

    Add : 전역 state 추가
    recoil 라이브러리 이용하여 isEdit과 isMake을 전역 state로 지정함

--- booklog/components/portfolio/PortfolioCard.tsx
+++ booklog/components/portfolio/PortfolioCard.tsx
@@ -1,9 +1,23 @@
+import { useRecoilState, 
+    useRecoilValue, 
+    useSetRecoilState, 
+    useResetRecoilState 
+  } from 'recoil';
+
+  import { isEditState } from "../../states/recoilBookPortfolio";
+
+
 const PortfolioCard = (props: any) => {
+    const [isEdit, setIsEdit] = useRecoilState<boolean>(isEditState);
+
     return (
         <>
         <div className="card">
             <div className="title">{props.title}</div>
             <div className="sub">{props.sub}</div>
+            {isEdit ?  <div className="btns"><button className="del">삭제</button><button className="alter">수정</button></div> : 
+            null
+            }           
         </div>
         <style jsx>{`
         .card{
@@ -32,6 +46,26 @@ const PortfolioCard = (props: any) => {
         .card:hover{
             background-color:#D8E3FC;
             box-shadow: 5px 5px gray;
+        }
+        .btns {
+            display:flex;
+            justify-content: flex-end;
+        }
+
+        button{
+            border:0px;
+            border-radius:10px;
+            color:white;
+            font-size:18px;
+            font-weight:600;
+            padding: 7px 18px;
+            margin-right:5px;
+        }
+        .del{
+            background-color:#F86258;
+        }
+        .alter{
+            background-color:#88BEE4;
         }
         `}</style>
     </>
